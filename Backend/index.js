d481df617b363366398e34fb69b58bcc05e3eac4
leandrotupas <tupas.leandro.lt@gmail.com>
2024-03-15 23:28:10 +0800

    started frontend development

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,21 @@ import mongoose from "mongoose";
 import express from 'express';
 import { Book } from "./Models/bookModels.js";
 import booksRoute from "./Routes/booksRoute.js";
+import cors from 'cors';
 
 const app = express();
 
 //Middleman to parse request body
 app.use(express.json());
+
+//Middleman to handle CORS Policy
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+);
 
 //Uses route from routes folder
 app.use('/books', booksRoute);
